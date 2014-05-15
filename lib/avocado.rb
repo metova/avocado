7cc50ddce2a13942618c32184d1f1f1fdbe50549
Logan Serman <logan.serman@metova.com>
2014-05-15 11:36:53 -0500

    allow_net_connect! when sending the yaml to a server

--- lib/avocado.rb
+++ lib/avocado.rb
@@ -17,11 +17,8 @@ module Avocado
 
   def self.upload!
     return if @payload.size.zero?
-
-    # write the file
-    File.open('avocado.yml', 'w+') do |file|
-      file.write payload.to_yaml
-    end
+    yaml = write_payload_to_yaml_file
+    WebMock.allow_net_connect!
 
     # read the file and upload it
     File.open('avocado.yml') do |file|
@@ -31,9 +28,10 @@ module Avocado
         http.request(req)
       end
     end
-
   rescue URI::InvalidURIError
     raise "Avocado::Config.url should point to your mounted Avocado documentation engine, it is currently not a valid URL"
+  ensure
+    WebMock.disable_net_connect!
   end
 
   def self.reset!
@@ -41,5 +39,13 @@ module Avocado
     Avocado::Config.reset!
     Avocado::Cache.clean
   end
+
+  private
+
+    def self.write_payload_to_yaml_file
+      File.open('avocado.yml', 'w+') do |file|
+        file.write payload.to_yaml
+      end
+    end
 
 end
